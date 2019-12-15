723f40475732412c7ab6d76b678c0c6e81024a3c
Ciprian <cipri686@gmail.com>
2019-12-15 18:48:25 +0100

    Creo que he acabado el nis_server(que por culo) y he empezado el nis_client

--- Practica/nis_server.sh
+++ Practica/nis_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 # Leemos los parametros de entrada
 fich_conf_ser=$1
 
@@ -17,41 +17,36 @@ fi
  #cambio de linea
  IFS=$'\n'
    
- 
-nr_linea=0;
-for nombre_dom in $(cat $fich_conf_ser)
-do
-	if [ $nr_linea -eq 0 ];
-	then
-		#Comprabos si esta el formato bien
-		[[ "$nombre_dom" != ?* ]] && echo "Formato de linea erroneo" && exit 1
-		nr_linea=1
-
-	else
-		echo "Error de formato en el fichero de configurccion de mount"
+ nr_lineas=$(cat $fich_conf_ser | wc -l)
+if [[ $nr_lineas -ne 1 ]]; then
+		echo "Formato de fichero de configuración erroneo xd"
 		exit 3
-	fi
-done
-IFS=$oldIFS
+else
+	#Porque raya a los chavales, ademas el ticches dijo que era pa parguelas
+	apt-get install -y systemd > /dev/null
+	systemctl disable NetworkManager.service > /dev/null
 
+	nombre_dom=$(head --lines=1 $fich_conf_ser)
+	#instalamos NIS
+	echo "--------------------- INSTALAMOS NIS ---------------------"
+	apt-get -q -y install nis > /dev/null    
 
-#instalamos NIS
-apt-get -q -y install nis > /dev/null    
-#Consifiguramos dominio
-echo $nombre_dom > /etc/defaultdomain  
+	#Consifiguramos dominio
+	echo "----------------- DEFINIMOS DOMINIO NIS ------------------"
+	domainname $nombre_dom  
+	echo "------------------ MODIFICAMOS FICHERPS -------------------"
+	#Especificamos el rol, modificamos el fichero /etc/default/nis
+	sed -e 's/NISSERVER=false/NISSERVER=master/; s/NISCLIENT=true/NISCLIENT=false/' /etc/default/nis > /dev/null
+	#Para que las contraseñas tambien esten el repositorio
+	sed -e 's/MERGE_PASSWD=false/MERGE_PASSWD=true/; s/MERGE_GROUP=false/MERGE_GROUP=true/' /var/yp/Makefile > /dev/null
+	#Volcamos el la info d econfiguración al erpositorio
+	echo "-------------- VOLVAMOS CONFIGURACION AL REPO ----------"
+	echo "#Ctrl+D\n" | /usr/lib/yp/ypinit -m &> /dev/null
 
-#Especificamos el rol, modificamos el fichero /etc/default/nis
+	#Arrancamos el servicio
+	
+	echo "----------------- ARRANCAMOS EL SERVICIO ------------------"
+	service nis start
+	echo "-----------------------SERV NIS UP------------------------"
 
-sed -i 's/NISSERVER=false/NISSERVER=master/'  /etc/default/nis 
-sed -i 's/NISCLIENT=true/NISCLIENT=true/'  /etc/default/nis 
-
-#Para que las contraseñas tambien esten el repositorio
-sed -i 's/MERGE_PASSWD=false/MERGE_PASSWD=true' /var/yp/Makefile
-sed -i 's/MERGE_GROUP=false/MERGE_GROUP=true' /var/yp/Makefile
-
-#Volcamos el la info d econfiguración al erpositorio
-/usr/lib/ypinit -m > /dev/null
-
-#Arrancamos el servicio
-systemctl restart nis
-
+fi
