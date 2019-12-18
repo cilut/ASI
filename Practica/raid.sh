694666c8af197f889cda4668630b26b8567dceae
Javier del Río <48770972+jadelriga@users.noreply.github.com>
2019-12-18 18:43:28 +0100

    Actualizado el script de raid

--- Practica/raid.sh
+++ Practica/raid.sh
@@ -1,86 +1,52 @@
 #!/bin/bash
 
-# Leemos los parametros de entrada
-fich_conf_ser=$1
-
-# Comprobamos que el numero de parametros
-# es el correcto
+#Numero de parametros
 if [ $# -ne 1 ]
 then
-	echo "			Numero de parametros incorrecto"
+	echo "Numero de parametros incorrecto"
 	exit 1
 fi
 
-	#Almacenamos el valor original de la variable IFS
- oldIFS=$IFS
- #Cambiamos el valor del IFS para que el delimitardor 
- #cambio de linea
- IFS=$'\n'
-   
- 
-nr_linea=0;
-for i in $(cat $fich_conf_ser)
-do
-	if [ $nr_linea -eq 0 ];	then
-
-		nombre_dispositivo_raid=$i
-		nr_linea=1
-
-	elif [ $nr_linea -eq 1 ]; then
-
-		nr_raid=$i
-		nr_linea=2
-	
-	elif [ $nr_linea -eq 2 ]; then
-
-		#Pillamos la linea de dispositivos y vamos si son dispositivos 
-		array_disp=$i
-		nr_elementos_array=${#array_disp[*]}
-		IFS=$oldIFS;
-		echo "Linea leida:"$i;
-		for j in $i; 
-		do
-
-			b=${j:5:3}
-			lsblk -fm | grep -w $b 
-			if [ $? -eq 1 ]; then
-				echo "No existe el dispositivo indicado"
-				exit 5
-			fi
-		done
-		nr_linea=3
-
-	else
-		echo "Error de formato en el fichero de configurccion de raid"
-		exit 4
-	fi
-done
-IFS=$oldIFS
-
-#Intentamos crear el array	
-mdadm --create --level=$nr_raid --raid-devices=$nr_elementos_array $nombre_dispositivo_raid $array_disp;	
-salida=$?
-if [[ salida -eq  127 ]]; then
-	apt-get update >/dev/null
-	apt-get -q --force-yes install mdadm > /dev/null
-
-	#Generamos el raid
-	mdadm --create --level=$nr_raid --raid-devices=$nr_elementos_array $nombre_dispositivo_raid $array_disp;
-	salida=$?
-	if [ salida -eq 2 ]; then
-		echo "Dispositivos indicados para la creacion usados o nombre incorrecto"
-		exit $salida
-	else
-		echo "Array generado satisfactoriamente"
-			#Hacemos permanete la configuración
-		mdadm --detail $nombre_dispositivo_raid --brief >> /etc/mdadm/mdadm.conf
-	fi
-else
-	echo "Array generado satisfactoriamente"
-	#Hacemos permanete la configuración
-	mdadm --detail $nombre_dispositivo_raid --brief >> /etc/mdadm/mdadm.conf
+#Numero de lineas debe ser igual a 3
+if [ $(wc -l $1 | mawk '{ print $1 }') -ne 3 ] #wc -l < $1
+then
+	echo "Formato de fichero incorrecto"
+	exit 2
 fi
 
+#Comprobar nivel de raid
+nivel=$(head -n 2 $1| tail -n 1)
+if [[ $nivel -gt 5 || $nivel -lt 0 ]] 
+then
+	echo "Nivel de raid incorrecto"
+	exit 3
+fi
 
-#Para eliminar raids mirar siguiente pagina:
-#https://redhatlinux.guru/2016/08/24/how-to-remove-mdadm-raid-devices/
+#Comprobar si existe un raid con ese nombre?
+
+#Comprobar si existe sistema de ficheros en los dispositivos? o en el primero solo?
+
+primerDisp=$(tail -n 1 $1 | mawk '{ print $1 }')
+
+if [ $(mount | grep $primerDisp | wc -l) -ne 0 ]
+then
+	echo "El dispositivo $primerDisp tiene un sistema de ficheros"
+	exit 4
+fi
+
+#Comprobar que mdadm esta instalado
+if [ $(which mdadm | wc -l) -eq 0 ]
+then
+	echo "Instalando la herramienta mdadm"
+	apt-get -y -q install mdadm > /dev/null
+	echo "La herramienta mdadm se ha instalado"
+else
+	echo "La herramienta mdadm ya se encuentra instalada"
+fi
+
+nDispositivos=$(tail -n 1 $1 | wc -w)
+nombre=$(head -n 1 $1)
+dispositivos=$(tail -n 1 $1)
+
+mdadm --create --level=$nivel --raid-devices=$nDispositivos $nombre $dispositivos > /dev/null
+exit 0
